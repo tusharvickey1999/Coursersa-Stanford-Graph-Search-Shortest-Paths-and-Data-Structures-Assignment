d268349ba57d8b5bbf6586e712088b4ad8d4d848
tusharvickey1999 <55626797+tusharvickey1999@users.noreply.github.com>
2020-09-19 15:04:39 +0530

    Update SCC.py

--- Week 1 Assignment/SCC.py
+++ Week 1 Assignment/SCC.py
@@ -1,3 +1,4 @@
+from collections import deque
 n=875714
 t=0
 f=[0 for i in range(0,n)]							# for storing finishing time.
@@ -18,31 +19,34 @@ def dfs(G,i):										#iterative dfs function because running recursive one is 
     global t
     global f
     global l
-    stk=[i]								                # for storing index whose edges has to be explored.
-    stk1=[]
+    stk=deque()							                # for storing index whose edges has to be explored.
+    stk.append(i)
+    stk1=deque()
     v[i]=1
     l[i]=s
     while(len(stk)!=0):
-        so=stk.pop(0)
-        stk1.insert(0,so)							        # need to store all the popped vertices to correctly give finishing time. not needed in case of recursive one.
+        so=stk.pop()
+        stk1.append(so)							        # need to store all the popped vertices to correctly give finishing time. not needed in case of recursive one.
         for j in range(0,len(G[so])):
             if v[G[so][j]]!=1:
                 v[G[so][j]]=1
                 l[G[so][j]]=s
-                stk.insert(0,G[so][j])
-    for i in stk1:    								        # using the stack1 to assign correct f[i] value
+                stk.append(G[so][j])
+                
+    while(len(stk1)!=0):                            # using the stack1 to assign correct f[i] value
+        i=stk1.pop()
         f[i]=t
         t+=1
 
 fo=open("foo.txt","+r")
 list1=fo.readlines()
 list2=[[int(i) for i in j.split()] for j in list1]
-print("69 mb file of only text phew....reading input completed... have patience...")
+print("69 mb file of only text phew....reading input completed...")
 Grev=[[] for i in range(0,n)]
 for i in list2:
     Grev[i[1]-1].append(i[0]-1)
 dfs_loop(Grev)
-print("First pass over... little more patience")    
+print("First pass over...")    
 G=[[] for i in range(0,n)]
 for i in range(0,n):
     for j in range(0,len(Grev[i])):
@@ -50,7 +54,7 @@ for i in range(0,n):
         G[Grev[i][j]].append(f[i])
 v=[0 for i in range(0,n)]
 dfs_loop(G)
-print("Second Pass completed... answer is on your way...")
+print("Second Pass completed...")
 le=[0 for i in range(0,n)]
 for i in l:
     le[i]+=1
